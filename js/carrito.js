895997d62eb639e52dee09bb44875578cc34b781
Emilse Erazo <emilsesilvana@gmail.com>
2024-05-14 11:36:16 -0300

    mejoras del carrito

---
+++ js/carrito.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const carrito = [];
+    const contenedorCarrito = document.querySelector('#carrito-productos');
+    const carritoVacio = document.querySelector('.carrito-vacio');
+  
+
